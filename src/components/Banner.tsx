373bfc0f09f205a36866e3ec09d59cd713f26362
Andy Huynh <andy4thehuynh@gmail.com>
2022-12-22 17:12:09 -0700

    ALH: Pulls the banner markup into a React component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+const Banner = () => {
+  return (
+    <div className="bg-red-500 text-center mx-auto">
+      <a className="bg-white text-xs text-center font-light no-underline" href="https://www.google.com/maps?q=950+Broadway,+Denver,+CO+80203,+USA&ftid=0x876c7f29c79cc459:0x331c561681c06022" target="_blank">
+        950 BROADWAY, DENVER, CO 80203
+      </a>
+      <a className="text-xs font-thin" href="tel:+3033350368">
+        (303) 335-0368
+      </a>
+    </div>
+  )
+}
+
+export default Banner;
