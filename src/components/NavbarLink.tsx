d3c4c2e4182e776c98d9e8acb3ed7f2acd507118
Andy Huynh <andy4thehuynh@gmail.com>
2022-12-30 20:00:05 -0700

    ALH: Add hover animation to navbarlinks

--- src/components/NavbarLink.tsx
+++ src/components/NavbarLink.tsx
@@ -1,7 +1,9 @@
+import "../styles/navbarlink.css"
+
 const NavbarLink = (props) => {
   return (
     <li className="inline-block mr-4">
-      <a href={props.url} className="block text-black font-semibold tracking-widest text-sm">
+      <a href={props.url} className="link-underline-animation block text-black font-semibold tracking-widest text-sm">
         {props.text}
       </a>
     </li>
