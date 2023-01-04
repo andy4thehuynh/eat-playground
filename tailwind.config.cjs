395090815bbf55a3ba0237d9ffa975fbd84e2a93
Andy Huynh <andy4thehuynh@gmail.com>
2023-01-04 14:06:07 -0700

    ALH: Adds Carousel with pictures centered in browser view

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+  plugins: [require('tw-elements/dist/plugin')],
 }
