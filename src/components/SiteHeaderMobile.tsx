6e58c296ca3e3f5906ab8c72f3f399a7ada357a5
Andy Huynh <andy4thehuynh@gmail.com>
2023-01-05 12:30:30 -0700

    ALH: Use margin to set spacing for banner in siteheadermobile

--- src/components/SiteHeaderMobile.tsx
+++ src/components/SiteHeaderMobile.tsx
@@ -1,12 +1,24 @@
+import LocationBanner from '../components/LocationBanner.js';
+
 const SiteHeaderMobile = () => {
   return (
-    <div className="block h-12 md:hidden">
-      <div className="float-right">
-        <button className="flex items-center px-3 py-2">
-          <svg className="fill-current h-7 w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></svg>
-        </button>
+    <section>
+      <div className="block h-12 md:hidden">
+        <div className="float-right">
+          <button className="flex items-center px-3 py-2">
+            <svg
+              className="fill-current h-7 w-7"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/>
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
+
+      <LocationBanner/>
+    </section>
   )
 }
 
