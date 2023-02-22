4ef7f6e7335c0e2e3348013c6fc4da9f7729fcfb
Andy Huynh <andy4thehuynh@gmail.com>
2023-02-22 12:46:25 -0700

    ALH: Add Foodmenu for mobile

--- src/components/FoodMenu.tsx
+++ src/components/FoodMenu.tsx
@@ -1,64 +1,54 @@
 const FoodMenu = () => {
   return (
-    <section className="grid gap-10 auto-cols-auto my-20 md:gap-4 md:grid-cols-6 md:mx-36 md:my-20">
-      <div className="flex items-center justify-center">
+    <section className="container mx-auto px-4 py-10">
+      <div className="flex flex-wrap items-center justify-around content-between">
         <a
           href="menu/pink-tiger-tacos/"
-          className="w-28 md:w-40">
+          className="w-20 m-2">
           <img
             src="/images/pink-tiger-tacos-logo.png"
             className="block"
-            alt="logo"/>
+            alt="pink tiger tacos logo"/>
         </a>
-      </div>
-      <div className="flex items-center justify-center">
         <a
           href="menu/kimbap/"
-          className="w-28 md:w-40">
+          className="w-20 m-2">
           <img
             src="/images/kimbap-logo.avif"
             className="block"
-            alt="logo"/>
+            alt="kimbap logo"/>
         </a>
-      </div>
-      <div className="flex items-center justify-center">
         <a
           href="menu/bonfire/"
-          className="w-28 md:w-40">
+          className="w-20 m-2">
           <img
             src="/images/bonfire-logo.png"
             className="block"
-            alt="logo"/>
+            alt="bonfire logo"/>
         </a>
-      </div>
-      <div className="flex items-center justify-center">
         <a
           href="menu/buddha-belly/"
-          className="w-28 md:w-40">
+          className="w-20 m-2">
           <img
             src="/images/buddha-belly-logo.png"
             className="block"
-            alt="logo"/>
+            alt="buddha belly logo"/>
         </a>
-      </div>
-      <div className="flex items-center justify-center">
         <a
           href="menu/gem-pan-asian/"
-          className="w-28 md:w-40">
+          className="w-20 m-2">
           <img
             src="/images/gem-pan-asian-logo.png"
             className="block"
-            alt="logo"/>
+            alt="gem pan asian logo"/>
         </a>
-      </div>
-      <div className="flex items-center justify-center">
         <a
           href="menu/bok-bok/"
-          className="w-28 md:w-40">
+          className="w-20 m-2">
           <img
             src="/images/bok-bok-logo.png"
             className="block"
-            alt="logo"/>
+            alt="bok bok logo"/>
         </a>
       </div>
     </section>
